4f06f1b5ec6667b14103b453c15d45a7eb1bdc77
Jao <joao.vfm18@gmail.com>
2021-09-25 08:08:00 -0400

    Criando tela de login

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,93 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Feirinha Virtual" />
+  <div class="box-border font-serif">
+    <div
+      class="
+        bg-green-600
+        p-2
+        mt-40
+        sm:w-1/2
+        sm:p-12
+        lg:w-2/5
+        xl:w-2/6
+        m-auto
+        text-center
+        rounded-tr-2xl rounded-bl-xl
+      "
+    >
+      <h1 class="text-5xl text-white m-auto">Feirinha Virtual</h1>
+      <div class="border-red p-8 m-auto outline-none">
+        <div class="flex justify-center">
+          <div class="flex flex-col">
+            <div class="flex flex-row">
+              <div
+                class="
+                  flex
+                  items-center
+                  p-2
+                  border-2 border-gray-500
+                  bg-gray-300
+                  rounded-tr-md
+                "
+              >
+                <ion-icon name="at-outline"></ion-icon>
+              </div>
+
+              <input
+                class="
+                  p-2
+                  rounded-tl-md rounded-br-md
+                  outline-none
+                  border-2 border-gray-500
+                  bg-gray-300
+                  
+                "
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+
+            <div class="flex flex-row">
+              <div
+                class="
+                  flex
+                  items-center
+                  p-2
+                  border-2 border-gray-500
+                  bg-gray-300
+                  rounded-tr-md
+                "
+              >
+                <ion-icon name="lock-closed-outline"></ion-icon>
+              </div>
+              <input
+                class="
+                  p-2
+                  rounded-tl-md rounded-br-md
+                  outline-none
+                  border-2 border-gray-500
+                  bg-gray-300
+                "
+                type="password"
+                placeholder="Senha"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full flex flex-row justify-center gap-2">
+        <app-btn> Entrar</app-btn>
+        <app-btn> Registrar-se</app-btn>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+import AppBtn from "./components/AppBtn.vue";
+export default {
+  name: "App",
+  components: {
+    AppBtn,
+  },
+};
+</script>
---
+++ src/components/AppBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button
+      class="
+        m-auto
+        text-white text-center text-2xl
+        h-12
+        w-40
+        border-2 border-solid
+        rounded-lg
+        hover:bg-white
+        hover:text-green-400
+      "
+    >
+      <slot> </slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'AppBtn',
+}
+</script>

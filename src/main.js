9832e9928c6bb0a53d41c24a92a58a4faf55e359
Alison Moura <alison.moura@hightechcursos.com.br>
2021-10-02 09:17:19 -0400

    Configurando VueRouter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './pages/Home.vue'
+import Profile from './pages/Profile.vue'
+import Login from './pages/Login.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    routes: [
+        { path: "/", component: Home },
+        { path: "/perfil", component: Profile },
+        { path: "/login", component: Login }
+    ],
+    history: createWebHistory()
+})
+
+const app = createApp(App) // Criação da aplicação Vue
+app.use(router) // Vue utilizar o VueRouter
+app.mount('#app') // Injeção do Vue
---
+++ src/pages/Login.vue
@@ -0,0 +1,128 @@
+<template>
+  <app-alert ref="alert">
+    {{ loginError }}
+  </app-alert>
+
+  <div class="box-border">
+    <div
+      class="
+        bg-green-400
+        p-2
+        mt-40
+        sm:w-1/2
+        sm:p-12
+        lg:w-2/5
+        xl:w-2/6
+        m-auto
+        text-center
+        rounded-md
+        shadow-lg
+      "
+    >
+      <h1 class="text-5xl text-white m-auto">Feirinha Virtual</h1>
+      <div class="border-red p-8 m-auto outline-none">
+        <div class="flex justify-center">
+          <div class="flex flex-col">
+            <div class="flex flex-row">
+              <div
+                class="
+                  flex
+                  items-center
+                  p-2
+                  border-2 border-gray-500
+                  bg-gray-300
+                  rounded-tr-md
+                "
+              >
+                <ion-icon name="at-outline"></ion-icon>
+              </div>
+
+              <input
+                class="
+                  p-2
+                  rounded-tl-md rounded-br-md
+                  outline-none
+                  border-2 border-gray-500
+                  bg-gray-300
+                "
+                type="email"
+                placeholder="Email"
+                v-model="user.email"
+              />
+            </div>
+
+            <div class="flex flex-row">
+              <div
+                class="
+                  flex
+                  items-center
+                  p-2
+                  border-2 border-gray-500
+                  bg-gray-300
+                  rounded-tr-md
+                "
+              >
+                <ion-icon name="lock-closed-outline"></ion-icon>
+              </div>
+              <input
+                class="
+                  p-2
+                  rounded-tl-md rounded-br-md
+                  outline-none
+                  border-2 border-gray-500
+                  bg-gray-300
+                "
+                type="password"
+                placeholder="Senha"
+                v-model="user.password"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full flex flex-row justify-center gap-2">
+        <app-button @click="login" class="bg-white text-green-400">
+          Entrar</app-button
+        >
+        <app-button class="bg-white text-green-400"> Registrar-se</app-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppButton from "./../components/AppButton.vue";
+import AppAlert from "./../components/AppAlert.vue";
+</script>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      user: {},
+      loginError: "",
+    };
+  },
+  methods: {
+    async login() {
+      // Request para a api
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/login",
+          this.user
+        );
+        alert("Usuário logado com sucesso");
+      } catch (error) {
+        // Exibir o alerta
+        this.$refs.alert.open();
+        this.loginError = error.response.data.message;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
